6792307a63f25d3fbbb059d8859ee5da877de659
John Stavast <stavastjohn@gmail.com>
2021-04-24 22:09:22 -0600

    javascript ordered list

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+const links = [
+    {
+        label: "Week 1 Notes",
+        url: "wk_01/index.html"
+    }
+]
+
+for (item of links){
+    let lbl = document.createElement('LI');
+    lbl.innerHTML = '<a href="' + item.url + '">' + item.label + '</a>';
+    document.querySelector('#mainOL').appendChild(lbl);
+}
