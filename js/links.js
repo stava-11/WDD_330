2e3e49e224eb96a018aaef4652deaf7a437d8ba1
John Stavast <stavastjohn@gmail.com>
2021-05-13 23:07:54 -0600

    main page and nav changes

--- js/links.js
+++ js/links.js
@@ -1,18 +1,18 @@
 const links = [
     {
         label: "Home",
-        url: ""
+        url: "WDD_330/"
     },
     {
         label: "Week 1 Notes",
-        url: "wk_01/index.html"
+        url: "WDD_330/wk_01/index.html"
     },
     {
         label: "Week 2 Notes",
-        url: "wk_02/index.html"
+        url: "WDD_330/wk_02/index.html"
     },
     {
         label: "Week 3 Notes",
-        url: "wk_03/index.html"
+        url: "WDD_330/wk_03/index.html"
     }
 ]
